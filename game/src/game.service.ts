d1d27bcc60d3b4bd1642c43d80206b99e0e9ebbf
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-15 19:47:25 +1100

    Added functions to my game server

--- game/src/game.service.ts
+++ game/src/game.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  private _selection? : 'rock' | 'paper' | 'scissors';
+
+  get selection(){
+    return this._selection;
+  }
+
   constructor() { }
+
+  selectedOption(option: 'rock' | 'paper' | 'scissors'){
+    this._selection = option;
+  }
+
+  comitSelection(){
+    if(this._selection == null){
+      alert('No option was selected');
+    }
+    
+
+  }
 }
