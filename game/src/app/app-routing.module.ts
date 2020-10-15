d923da3d1f3a6adddee49eaf9f28169b1492e920
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-15 12:42:44 +1100

    Added route paths and outlet

--- game/src/app/app-routing.module.ts
+++ game/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayComponent } from './routes/play/play.component';
+import { ResultsComponent } from './routes/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "play", component: PlayComponent},
+  {path: "result", component: ResultsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- game/src/app/app.module.ts
+++ game/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayComponent } from './routes/play/play.component';
+import { ResultsComponent } from './routes/results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
