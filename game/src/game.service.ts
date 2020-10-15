d4f095537fc59f2364f442858f7ec446f05d9133
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-15 20:05:40 +1100

    Completed commiting selection to see results

--- game/src/game.service.ts
+++ game/src/game.service.ts
@@ -1,29 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  result: 'Win' | 'Loose';
   private _selection? : 'rock' | 'paper' | 'scissors';
 
   get selection(){
     return this._selection;
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  get computerSelection(){
+    return 'scissors';
+  }
 
   selectedOption(option: 'rock' | 'paper' | 'scissors'){
     this._selection = option;
   }
 
   comitSelection(){
-    if(this._selection == null){
-      alert('No option was selected');
-    }
-    
+    of(null).pipe(delay(1000)).subscribe(() => {
+      if(this._selection == null){
+        alert('No option was selected');
+      }
 
+      this.router.navigateByUrl('/results');
+
+    });
   }
+
 }
