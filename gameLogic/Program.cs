88a2ec4b44e3ff4e15df69c8e29e2ec0e9e17a7a
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-19 11:06:11 +1100

    Starting game logic development

---
+++ gameLogic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gameLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
