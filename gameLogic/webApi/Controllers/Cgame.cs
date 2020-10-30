1342bc928b3a3b5b9426f12ffa97c1ef0aa2a99e
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-30 11:01:52 +1100

    returned to Release 1 API (Almost)

--- gameLogic/webApi/Controllers/Cgame.cs
+++ gameLogic/webApi/Controllers/Cgame.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using gameClass;
 using player;
-using userClass;
 
 namespace webApi.Controllers
 {
@@ -12,8 +11,6 @@ namespace webApi.Controllers
     [Route("[controller]")]
     public class CgameController : ControllerBase
     {
-        public List<User> Leaderboard = new List<User>();
-
         [JsonProperty("GamePlayed")]
         public Game GamePlayed { get; set; }
 
@@ -26,48 +23,7 @@ namespace webApi.Controllers
         public Game determineWinner([FromBody] Player p)
         {
             this.GamePlayed.getGameResultAgainstCPU(p);
-            // if (this.Leaderboard.Count > 0)
-            // {
-            //     foreach (User user in Leaderboard)
-            //     {
-            //         if (user.username == p.UserName)
-            //         {
-            //             user.gamesPlayed++;
-            //             if (this.GamePlayed.GameResult == "Win")
-            //             {
-            //                 user.gamesWon++;
-            //             }
-            //         }
-            //         else
-            //         {
-            //             var newUser = new userClass.User(p.UserName);
-            //             newUser.gamesPlayed++;
-            //             if (this.GamePlayed.GameResult == "Win")
-            //             {
-            //                 newUser.gamesWon++;
-            //             }
-            //             this.Leaderboard.Add(newUser);
-            //         }
-            //     }
-            // }else{
-            //     var newUser = new userClass.User(p.UserName);
-            //     newUser.gamesPlayed++;
-            //     if (this.GamePlayed.GameResult == "Win")
-            //     {
-            //         newUser.gamesWon++;
-            //     }
-            //     this.Leaderboard.Add(newUser);
-            // }
-            
-            this.Leaderboard.Add(new userClass.User(p.UserName));
-
             return this.GamePlayed;
-        }
-
-        [HttpGet("Leaderboard")]
-        public List<User> getLeaderboard()
-        {
-            return this.Leaderboard;
         }
 
     }
