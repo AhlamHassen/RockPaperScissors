dfc04f0db221653c4452f2d61b0dd3b796921441
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-11-05 11:33:28 +1100

    Copied & editted the play c html & css into roundC

--- game/src/app/app-routing.module.ts
+++ game/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { PlayComponent } from './routes/play/play.component';
 import { ReportComponent } from './routes/report/report.component';
 import { ResultsComponent } from './routes/results/results.component';
+import { RoundsComponent } from './routes/rounds/rounds.component';
 
 const routes: Routes = [
   {path: "play", component: PlayComponent},
   {path: "results", component: ResultsComponent},
   {path: "report", component: ReportComponent},
+  {path: "rounds", component: RoundsComponent},
 
-  {path: "**", redirectTo: "play"}
+  {path: "**", redirectTo: "rounds"}
 ];
 
 @NgModule({
--- game/src/app/routes/rounds/rounds.component.ts
+++ game/src/app/routes/rounds/rounds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/game.service';
 
 @Component({
   selector: 'app-rounds',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rounds.component.css']
 })
 export class RoundsComponent implements OnInit {
+  userName = '';
+  oneRound : boolean = false;
+  threeRounds: boolean = false;
+  fiveRounds: boolean = false;
 
-  constructor() { }
+  constructor(public gameservice: GameService) { }
 
   ngOnInit(): void {
+  }
+
+  optionClicked(option: 'oneRound' | 'threeRounds' | 'fiveRounds'){
+    if(option == 'oneRound'){
+      this.oneRound = this.oneRound? false : true;
+      
+      this.threeRounds = false;
+      this.fiveRounds = false;
+
+    }else if(option == 'threeRounds'){
+      this.threeRounds = this.threeRounds? false : true;
+
+      this.oneRound = false;
+      this.fiveRounds = false;
+    }else{
+      this.fiveRounds = this.fiveRounds? false: true;
+
+      this.oneRound = false;
+      this.threeRounds = false;
+    }
+
+    if(this.oneRound != false || this.threeRounds != false || this.fiveRounds != false){
+      //
+    }
+
+  }
+
+  onKey(value: any){
+    this.userName = value;
+    this.gameservice.getUsername(this.userName);
+    if(this.userName != null || this.userName != ''){
+      localStorage.setItem('username', JSON.stringify(this.userName));
+    }
+  }
+
+  buttonClicked(){
+    if(this.userName == '' && localStorage.getItem('username') == null){
+      alert("There was no username entered");
+      return;
+    }
+    else if(localStorage.getItem('username') != null && this.userName == ''){
+      alert("There was no username entered");
+      return; 
+    }
+  
+    if(this.oneRound == false && this.threeRounds == false && this.fiveRounds == false){
+      alert('No option was selected');
+      return;
+    }
+    // this.gameservice.comitSelection();
+    // localStorage.setItem('playerSelection', JSON.stringify(this.gameservice.selection));
+    
   }
 
 }
