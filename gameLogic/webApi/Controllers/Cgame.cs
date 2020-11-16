708158e3b932bb98e20445ac315e166cdad34099
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-11-16 11:33:48 +1100

    Completed Release 1.1

--- gameLogic/webApi/Controllers/Cgame.cs
+++ gameLogic/webApi/Controllers/Cgame.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using gameClass;
 using player;
 using LeaderboardLine;
@@ -12,31 +13,56 @@ namespace webApi.Controllers
     [Route("[controller]")]
     public class CgameController : ControllerBase
     {
-        static List<LeaderbrdLine> Leaderboard = new List<LeaderbrdLine>(){
-            new LeaderbrdLine("amiiy123", 52, 60),
-            new LeaderbrdLine("asd123", 45, 87),
-            new LeaderbrdLine("ram2000", 44, 82)
-        };
+        List<LeaderbrdLine> Leaderboard = new List<LeaderbrdLine>();
 
         [JsonProperty("GamePlayed")]
         public Game GamePlayed { get; set; }
 
+        [JsonProperty("Player")]
+        public Player Player { get; set; }
+        
+
         public CgameController()
         {
             this.GamePlayed = new Game();
+            this.Player = new Player();
         }
 
         [HttpPost("PostSelection")]
         public Game determineWinner([FromBody] PlayerSelection p)
         {
-            //if this p.username is already in a database then just play else insert them into the player table
+            //if this user is already in the database then just play else insert them into the player table
+            if(this.Player.doesExist(p) == null){
+                this.Player.addPlayer(p);
+            }
+
+            //insert this game into the game table in the database
             this.GamePlayed.getGameResultAgainstCPU(p);
-            //insert this game into the game/turn table in the database
+            this.GamePlayed.addGame(this.GamePlayed);
+
             return this.GamePlayed;
         }
 
         [HttpGet("Leaderboard")]
         public List<LeaderbrdLine> getLeaderboard(){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=TEST;User ID=admin; Password=kereneritrea";
+            SqlConnection con = new SqlConnection(connectionString);
+            
+            string queryString = "SELECT * FROM Leaderboard ORDER BY [Win Ratio] DESC";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            con.Open();
+
+            using(SqlDataReader reader = command.ExecuteReader()){
+                 while(reader.Read()){
+
+                    Leaderboard.Add(
+                        new LeaderbrdLine(reader[0].ToString(), (int)reader[1], (int)reader[2])
+                    );
+                }
+            }
+
             return Leaderboard;
         }
 
