4f1d2034c3383e9192ccf55b001d4f5e5cbb4725
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-11-16 11:33:08 +1100

    Methods to check if player exists in db & add 2 db

--- gameLogic/player/Player.cs
+++ gameLogic/player/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
 
 namespace player
 {
@@ -10,6 +11,46 @@ namespace player
 
         public Player(string username){
             this.UserName = username;
+        }
+
+        public Player(){
+            this.UserName = "";
+        }
+
+        public Player doesExist(PlayerSelection p){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=TEST;User ID=admin; Password=kereneritrea";
+            SqlConnection con = new SqlConnection(connectionString);
+            
+            string queryString = "SELECT * FROM Player WHERE userName = @user";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@user", p.UserName);
+            con.Open();
+            Player found = null;
+
+            using(SqlDataReader reader = command.ExecuteReader()){
+                while(reader.Read())
+                {
+                   found = new Player(reader[0].ToString());
+           
+                }
+            }
+
+            return found; 
+        }
+
+        public void addPlayer(PlayerSelection p){
+            string connectionString = @"Data Source=rpsdp.ctvssf2oqpbl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=TEST;User ID=admin; Password=kereneritrea";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "INSERT INTO Player (userName) VALUES (@user)"; 
+            
+            SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@user", p.UserName);
+            con.Open();
+            command.ExecuteNonQuery();
         }
     }
 }
